6b04658bcd4e5ab4daa76d5390f74ab1c0fbc9d9
nyatadecocoa <yoshii.y.ad@m.titech.ac.jp>
2020-07-05 02:09:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Dixit 改 (Dixit Akira)
+
+## 開発環境
+
+- 言語はSwift
+- IDEはXCode
+
+## メンバー
+
+- Akira Watanabe
+- Jumpei Yoshikawa
+- Yasuaki Yoshii
+
+## フレームワーク
+
+ガチで作るとすれば，Metalを用いたいところだが，
+残念ながら我々はそこまでの腕がないので大人しく
+SpriteKitを用いることにする．
+
+## ファイル構成
+
+- resoureces : 画像等のリソース群
+- dixit : swiftコード等のコード群
+- dixitTests : テスト用コード
+- dixitUITests : UI用のテストコード
+- Products : ビルド後の実行コード(.appなど)群
---
+++ dixit/GameScene.swift
@@ -0,0 +1,128 @@
+//
+//  GameScene.swift
+//  dixit
+//
+//  Created by Yasuaki Yoshii on 2020/07/04.
+//  Copyright © 2020 Yasuaki Yoshii. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func sceneDidLoad() {
+        // シーンが表示されるタイミングで呼び出される
+        // 主に初期化
+        print("[debug] sceneDidLoad - called.")
+        self.lastUpdateTime = 0
+        
+        // Get label node from scene and store it for use later
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
+        // set akira
+        let akira : SKSpriteNode = SKSpriteNode(imageNamed: "akira_with_Ginkakuji.HEIC")
+        let unko : SKSpriteNode = SKSpriteNode(imageNamed: "unko.gif")
+        // resize akira
+        akira.size.height *= (self.frame.size.width / akira.size.width)
+        akira.size.width = self.frame.size.width
+        self.addChild(akira)
+        self.addChild(unko)
+        
+
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.removeFromParent()]))
+        }
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        print("[debug] touchDown - called.")
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        print("[debug] touchMoved - called.")
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        print("[debug] touchUp - called.")
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    /** 画面をタッチ開始したときに呼ばれる*/
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("[debug] touchesBegun - called.")
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("[debug] touchesMoved - called.")
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    /** 指が離れたときに呼び出される*/
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
